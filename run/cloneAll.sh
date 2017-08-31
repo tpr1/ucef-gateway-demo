62068c561da6f1d01b439422f397edc05870e971
gcr-nist <geoffry.roberts@nist.gov>
2017-08-31 16:51:58 +0000

    Add file cloneAll.sh

--- run/cloneAll.sh
+++ run/cloneAll.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
-cd ../../
 
+cd ../../
+echo `pwd`
 git clone https://github.com/usnistgov/ucef-core.git
+cd ucef-core
+echo `pwd`
 git checkout feature/uber
+cd ..
+echo `pwd`
 git clone https://github.com/usnistgov/ucef-gateway.git
+cd ucef-gateway
+echo `pwd`
 git checkout develop
-git clone https://github.com/usnistgov/ucef-gateway-demo.git
+cd ..
+echo `pwd`
 git clone https://github.com/gcr-nist/TripleFederation_generated.git
+cd TripleFederation_generated
+echo `pwd`
+cd ..
+echo `pwd`
+
