d8875b925623f38735c10bae711577c6cdeae6b9
gcr-nist <geoffry.roberts@nist.gov>
2017-09-19 12:12:15 +0000

    Remove checkout statements

--- run/cloneAll.sh
+++ run/cloneAll.sh
@@ -5,13 +5,11 @@ echo `pwd`
 git clone https://github.com/usnistgov/ucef-core.git
 cd ucef-core
 echo `pwd`
-git checkout feature/uber
 cd ..
 echo `pwd`
 git clone https://github.com/usnistgov/ucef-gateway.git
 cd ucef-gateway
 echo `pwd`
-git checkout develop
 cd ..
 echo `pwd`
 git clone https://github.com/gcr-nist/TripleFederation_generated.git
