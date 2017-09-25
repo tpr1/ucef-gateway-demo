214cba57b96e03c45dfd12bb4004d5277710bf75
geoffry.roberts <geoffry.roberts@nist.gov>
2017-09-25 18:12:35 +0000

    Add checkout branch to cloneAll.sh

--- run/cloneAll.sh
+++ run/cloneAll.sh
@@ -3,11 +3,13 @@
 cd ../../
 echo `pwd`
 git clone https://github.com/usnistgov/ucef-core.git
+git checkout develop
 cd ucef-core
 echo `pwd`
 cd ..
 echo `pwd`
 git clone https://github.com/usnistgov/ucef-gateway.git
+git checkout develop
 cd ucef-gateway
 echo `pwd`
 cd ..
